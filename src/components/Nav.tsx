4e4c372576d080febf8567bbc4ce49299fc25d2e
José Vitor dos Santos Fernandes <jose-vitor-7@hotmail.com>
2024-04-10 23:07:21 -0300

    fix: nav link hover and link component

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,27 +1,29 @@
+import Link from 'next/link'
+
 export const Nav: React.FC = () => {
   return (
     <nav className="fixed top-0 z-10 flex w-full items-center justify-between p-8 mix-blend-difference">
       <div className="logo">
-        <a
+        <Link
           href="#"
           className="font-[Humane,_sans-serif] text-7xl uppercase text-white mix-blend-difference [text-decoration:none]"
         >
           OUTFLOW
-        </a>
+        </Link>
       </div>
       <div className="links flex items-center gap-4">
-        <a
+        <Link
           href="#"
           className="rounded-full border-[1px] border-white px-4 pb-2 pt-2.5 text-sm uppercase text-white transition-all [text-decoration:none] hover:bg-white hover:text-black"
         >
           JOIN COMMUNITY
-        </a>
-        <a
+        </Link>
+        <Link
           href="#"
-          className="rounded-full border-[1px] border-white px-4 pb-2 pt-2.5 text-sm uppercase text-white [text-decoration:none]"
+          className="rounded-full border-[1px] border-white px-4 pb-2 pt-2.5 text-sm uppercase text-white transition-all [text-decoration:none] hover:bg-white hover:text-black"
         >
           ENROLL NOW
-        </a>
+        </Link>
       </div>
     </nav>
   )
